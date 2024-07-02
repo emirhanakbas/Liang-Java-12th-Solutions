0af03d8c9ba6dc1800d3f56271d298428511198e
Emirhan Akbaş <125372641+emirhanakbas@users.noreply.github.com>
2024-07-02 19:05:00 +0300

    Update Exercise01_13.java

--- Chapter01/src/Exercise01_13.java
+++ Chapter01/src/Exercise01_13.java
@@ -10,8 +10,8 @@
 public class Exercise01_13 {
     public static void main(String[] args) {
         // Display x
-        System.out.println( (44.5 * 0.55 - 50.2 * 5.9 ) / ( 3.4 * 0.55 - 50.2 * 2.1 ) );
+        System.out.println( ( 44.5 * 0.55 - 50.2 * 5.9 ) / ( 3.4 * 0.55 - 50.2 * 2.1 ) );
         // Display y
-        System.out.println( (3.4 * 5.9 - 44.5 * 2.1 ) / ( 3.4 * 0.55 - 50.2 * 2.1 ) );
+        System.out.println( ( 3.4 * 5.9 - 44.5 * 2.1 ) / ( 3.4 * 0.55 - 50.2 * 2.1 ) );
     }
 }
